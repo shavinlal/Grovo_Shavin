e1869f5632b549df42a9093fb5861bd085fb3cef
QAutomat <36909286+QAutomat@users.noreply.github.com>
2018-03-02 15:48:37 +0530

    Update BaseTestClass.py

--- TestCases/BaseTestClass.py
+++ TestCases/BaseTestClass.py
@@ -1,13 +1,77 @@
+'''
+Created on 21-Feb-2018
+
+@author: Shilpa
+'''
+import time
+import xlrd
+import os.path
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
-# create a new Firefox session
-driver=webdriver.Chrome("chromedriver")
-driver.implicitly_wait(30)
-driver.maximize_window()
+driver=webdriver.Chrome('chromedriver')
 
-# navigate to the application home page
-driver.get("https://optislabs-playpen.grovo.com/")
+class BaseTestClass:
+    
+    def userLogin(self):
+    
+        print "Opening Browser"
+        
+        driver.maximize_window()
+        
+        print "Reading data from excel sheet"
+        book=xlrd.open_workbook(os.path.join('./TestData.xlsx'))
+        
+        # print number of sheets
+        print("Number of WorkSheets :")
+        print book.nsheets
+    
+        # print sheet names
+        #print("Name of WorkSheets :")
+        #print book.sheet_names()
+        
+        # get the first worksheet
+        first_sheet = book.sheet_by_name('Login_Credentials')
+        
+        # read a row
+        #print("First Row Data in 1st WorkSheet :")
+        #print first_sheet.row_values(0)
+        
+        print("Fetching the URL, username and password from Excel Sheet\n")
+        # read a cell
+        cell = first_sheet.cell(1,1)
+        url = cell.value
+        print url
+        
+        cell = first_sheet.cell(3,1)
+        username = cell.value
+        print username
+        
+        cell = first_sheet.cell(3,2)
+        password = cell.value
+        print password  
+        
+        driver.get(url)
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "password")))
+        if driver.title == "Grovo":
+            print("Grovo Application URL Opened")
+        else:
+            raise Exception.message
 
-# close the browser window
-driver.quit()
+        print "Grovo Sign-In page is displayed"
+        
+        print "Entering User name"
+        driver.find_element_by_xpath(".//*[@id='username']").send_keys(username)
+       
+        print "Entering Password"
+        element.send_keys(password)
+        
+        element.send_keys(Keys.TAB)
+        print "Clicking on Sign_In button"
+        driver.find_element_by_xpath("//*[@id='submitButton']").click()
+        
+        print "Successfully Loged Into Grovo Application"
+        time.sleep(5)
