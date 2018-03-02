de688f30ea7db7934e7744307ad1ef83b47ad20e
QAutomat <36909286+QAutomat@users.noreply.github.com>
2018-03-02 14:34:36 +0530

    Update BaseTestClass.py

--- TestCases/BaseTestClass.py
+++ TestCases/BaseTestClass.py
@@ -1,83 +1,38 @@
-'''
-Created on 21-Feb-2018
-
-@author: Shilpa
-'''
-import time
-import xlrd
-import os.path
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 
-driver=webdriver.Chrome("/Users/admin/Documents/chromedriver")
+# create a new Firefox session
+driver = webdriver.Firefox()
+driver.implicitly_wait(30)
+driver.maximize_window()
 
-class BaseTestClass:
-    
-    def userLogin(self):
-    
-        print "Opening Browser"
-        
-        driver.maximize_window()
-        
-        print "Reading data from excel sheet"
-        book=xlrd.open_workbook(os.path.join('TestData.xlsx'))
-        
-        # print number of sheets
-        print("Number of WorkSheets :")
-        print book.nsheets
-    
-        # print sheet names
-        #print("Name of WorkSheets :")
-        #print book.sheet_names()
-        
-        # get the first worksheet
-        first_sheet = book.sheet_by_name('Login_Credentials')
-        
-        # read a row
-        #print("First Row Data in 1st WorkSheet :")
-        #print first_sheet.row_values(0)
-        
-        print("Fetching the URL, username and password from Excel Sheet\n")
-        # read a cell
-        cell = first_sheet.cell(1,1)
-        url = cell.value
-        print url
-        
-        cell = first_sheet.cell(3,1)
-        username = cell.value
-        print username
-        
-        cell = first_sheet.cell(3,2)
-        password = cell.value
-        print password  
-        
-        driver.get(url)
-        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "password")))
-        if driver.title == "Grovo":
-            print("Grovo Application URL Opened")
-        else:
-            raise Exception.message
+# navigate to the application home page
+driver.get("http://www.google.com")
 
-        print "Grovo Sign-In page is displayed"
-        
-        print "Entering User name"
-        driver.find_element_by_xpath(".//*[@id='username']").send_keys(username)
-       
-        print "Entering Password"
-        element.send_keys(password)
-        
-        element.send_keys(Keys.TAB)
-        print "Clicking on Sign_In button"
-        driver.find_element_by_xpath("//*[@id='submitButton']").click()
-        
-        print "Successfully Loged Into Grovo Application"
-        time.sleep(5)
-        
-        
+# get the search textbox
+search_field = driver.find_element_by_id("lst-ib")
+search_field.clear()
 
+# enter search keyword and submit
+search_field.send_keys("Selenium WebDriver Interview questions")
+search_field.submit()
 
+# get the list of elements which are displayed after the search
+# currently on result page using find_elements_by_class_name  method
+lists= driver.find_elements_by_class_name("_Rm")
 
-    
+# get the number of elements found
+print (“Found “ + str(len(lists)) + “searches:”)
+
+# iterate through each element and print the text that is
+# name of the search
+
+i=0
+for listitem in lists:
+   print (listitem)
+   i=i+1
+   if(i>10):
+      break
+
+# close the browser window
+driver.quit()
