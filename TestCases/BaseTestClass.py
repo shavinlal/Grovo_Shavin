c708e173a619f6a87105854abeea75e7c8c47455
QAutomat <36909286+QAutomat@users.noreply.github.com>
2018-03-02 14:42:45 +0530

    Update BaseTestClass.py

--- TestCases/BaseTestClass.py
+++ TestCases/BaseTestClass.py
@@ -2,34 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 # create a new Firefox session
-driver = webdriver.Firefox()
+driver=webdriver.Chrome("/TestCases/chromedriver")
 driver.implicitly_wait(30)
 driver.maximize_window()
 
 # navigate to the application home page
-driver.get("http://www.google.com")
-
-# get the search textbox
-search_field = driver.find_element_by_id("lst-ib")
-search_field.clear()
-
-# enter search keyword and submit
-search_field.send_keys("Selenium WebDriver Interview questions")
-search_field.submit()
-
-# get the list of elements which are displayed after the search
-# currently on result page using find_elements_by_class_name  method
-lists= driver.find_elements_by_class_name("_Rm")
-
-# iterate through each element and print the text that is
-# name of the search
-
-i=0
-for listitem in lists:
-   print (listitem)
-   i=i+1
-   if(i>10):
-      break
+driver.get("https://optislabs-playpen.grovo.com/")
 
 # close the browser window
 driver.quit()
